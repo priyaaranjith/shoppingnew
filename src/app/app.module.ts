4185d3ceed399ce6a44fd44457792391a0ba92d8
priyaaranjith <ranjithpriya307@gmail.com>
2022-12-08 09:53:08 +0530

    admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminloginComponent } from './adminlogin/adminlogin.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoute : Routes = [
+  {
+    path : "",
+    component : AdminloginComponent
+  }
+]
+
 
 @NgModule({
   declarations: [
@@ -14,7 +23,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
